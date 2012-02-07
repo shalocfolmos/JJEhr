df9815193271b51cc74ae512b58585359d6fb55e
shalocfolmos <shalocfolmos@gmail.com>
2012-02-07 11:45:04 +0800

    add template

--- JJEhr/settings.py
+++ JJEhr/settings.py
@@ -102,6 +102,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'JJEhr.urls'
 
 TEMPLATE_DIRS = (
+    "./lesson/template"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
