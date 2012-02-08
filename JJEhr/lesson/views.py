d1b37150ffa271fa22d9130045680dc732fa4dc2
shalocfolmos <shalocfolmos@gmail.com>
2012-02-08 14:17:22 +0800

    remove unwanted template

--- JJEhr/lesson/views.py
+++ JJEhr/lesson/views.py
@@ -1,5 +1,12 @@
 # Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from django.template import Context,Template, context
+
 def index(httpRequest):
-    return render_to_response('lesson/index.html')
+    t = Template("{{name}}")
+    c=Context({"name":"a"})
+    return HttpResponse(t.render(c))
+
+#    return render_to_response(t.render(c))
--- JJEhr/settings.py
+++ JJEhr/settings.py
@@ -19,6 +19,8 @@ DATABASES = {
     }
 }
 
+TEMPLATE_STRING_IF_INVALID= 'B'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -44,7 +46,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/folmos/courseware'
+MEDIA_ROOT = './courseware'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
