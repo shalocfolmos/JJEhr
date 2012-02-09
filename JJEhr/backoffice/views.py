30fa445dde3fa71053f2e25ad879190345c0baea
shalocfolmos <shalocfolmos@gmail.com>
2012-02-09 17:52:30 +0800

    添加backoffice模块,并移除admin模版

--- JJEhr/backoffice/views.py
+++ JJEhr/backoffice/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from JJEhr.lesson.models import Course
 
 def test(httpRequest):
-    return HttpResponse("ok")
+    return render_to_response('backoffice/index.html',{'course',Course.objects.get(id=1)})
--- JJEhr/lesson/admin.py
+++ JJEhr/lesson/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from JJEhr.lesson.models import Course
+from JJEhr.lesson.models import Course, Enroll
 
 
 admin.site.register(Course)
+admin.site.register(Enroll)
