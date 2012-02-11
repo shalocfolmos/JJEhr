6218765275ef9097638839a6fd647cd99434ebd9
root <root@localhost.localdomain>
2012-02-11 17:25:54 +0800

    change waiting list logic

--- JJEhr/settings.py
+++ JJEhr/settings.py
@@ -74,7 +74,8 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "./static/backoffice",
+    "./static",
+    "./static/yui"
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
