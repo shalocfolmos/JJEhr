d4dcecf814a4503ffc356b03cc1cee9de2a86b00
sam.sun <shalocfolmos@gmai.com>
2012-02-18 15:04:36 +0800

    添加add course style

--- JJEhr/urls.py
+++ JJEhr/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from JJEhr import backoffice
 
 admin.autodiscover()
 
@@ -18,21 +17,20 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #url(r'^backoffice/index.html','backoffice.views.index'),
-    url(r'^$','lesson.views.index'),
-    url(r'^(\d+)/$','lesson.views.detail'),
-    url(r'^book/$','lesson.views.book_course'),
-    url(r'^course/index.html','lesson.views.index'),
-    url(r'^course/download/','lesson.views.download'),
+    url(r'^$', 'lesson.views.index'),
+    url(r'^book/$', 'lesson.views.book_course'),
+    url(r'^course/index.html', 'lesson.views.index'),
+    url(r'^course/download/', 'lesson.views.download'),
     #url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^backoffice/login', 'django.contrib.auth.views.login',{'template_name':'backoffice/login.html'}),
+    url(r'^backoffice/login', 'django.contrib.auth.views.login', {'template_name': 'backoffice/login.html'}),
     url(r'^backoffice/index.html', 'backoffice.views.displayCourseList'),
     url(r'^backoffice/course/(?P<courseId>\d+)$', 'backoffice.views.courseView'),
     url(r'^backoffice/deletecourse/(?P<courseId>\d+)$', 'backoffice.views.delete_course'),
     url(r'^backoffice/logout', 'backoffice.views.admin_logout'),
     url(r'^backoffice/course/add', 'backoffice.views.addCourse'),
-
-#    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+    #    url(r'^admin/', include(admin.site.urls))
 )
 
 urlpatterns += staticfiles_urlpatterns()
