d9543a1b0ca3f49f09fe88ca37a3eb13da6f45b5
sam.sun <shalocfolmos@gmai.com>
2012-02-20 22:08:51 +0800

    修改前台央视

--- JJEhr/lesson/views.py
+++ JJEhr/lesson/views.py
@@ -3,7 +3,7 @@
 import datetime
 import os
 from django.core.servers.basehttp import FileWrapper
-from django.shortcuts import render_to_response,get_object_or_404
+from django.shortcuts import render_to_response
 from lesson.manager import tempMapping
 import settings
 from JJEhr.lesson.models import Course, Enroll, EnrollForm
@@ -14,25 +14,25 @@ from django.template.context import RequestContext
 def index(httpRequest):
     kwargs = httpRequest.GET
     isSearch = False
-    if kwargs.has_key('searchType') and kwargs.has_key('searchContent') :
+    if kwargs.has_key('searchType') and kwargs.has_key('searchContent'):
         current_search_type = tempMapping().getSearchType(kwargs.get('searchType'))
         searchContent = kwargs.get('searchContent')
         isSearch = True
-    course_set = Course.search_objects.search(** kwargs)
-    allow_course_id=[]
+    course_set = Course.search_objects.search(**kwargs)
+    allow_course_id = []
     for course in course_set.object_list:
-        if course.enrollStartTime < datetime.datetime.now() and course.enrollEndTime > datetime.datetime.now() :
+        if course.enrollStartTime < datetime.datetime.now() and course.enrollEndTime > datetime.datetime.now():
             allow_course_id.append(course.id)
     _context = {
-        'course_list' :course_set,
-        'allow_course_id_list':allow_course_id,
-        'search_type':tempMapping().getAllSearchType(),
-        'isSearch' : isSearch,
-    }
+        'course_list': course_set,
+        'allow_course_id_list': allow_course_id,
+        'search_type': tempMapping().getAllSearchType(),
+        'isSearch': isSearch,
+        }
     if isSearch:
         _context['current_search_type'] = current_search_type
         _context['searchContent'] = searchContent
-    return render_to_response('lesson/index1.html', _context ,context_instance=RequestContext(httpRequest))
+    return render_to_response('lesson/index1.html', _context, context_instance=RequestContext(httpRequest))
 
 #def detail(request, id):
 #    try:
@@ -57,16 +57,16 @@ def book_course(request):
         if form.is_valid():
             _email = form._raw_value('email')
             _course = Course.objects.get(id=form._raw_value("course_id"))
-            if _course.enrollStartTime > datetime.datetime.now() or _course.enrollEndTime < datetime.datetime.now() :
+            if _course.enrollStartTime > datetime.datetime.now() or _course.enrollEndTime < datetime.datetime.now():
                 return HttpResponse(404)
-            enroll_count = Enroll.objects.filter(email = _email, course = _course).count()
+            enroll_count = Enroll.objects.filter(email=_email, course=_course).count()
             if enroll_count > 0:
                 return HttpResponse(409)
             else:
                 enroll = Enroll(email=_email, course=_course)
                 successEnrollMemberCount = Enroll.objects.filter(isWaitingList=False).count()
-                if successEnrollMemberCount>=_course.maxTraineeAmount:
-                    enroll.isWaitingList=True
+                if successEnrollMemberCount > _course.maxTraineeAmount:
+                    enroll.isWaitingList = True
                 enroll.save()
             return HttpResponse(200)
         else:
@@ -74,13 +74,14 @@ def book_course(request):
     else:
         return HttpResponse(403)
 
+
 def download(request):
     filename = request.GET['file']
     showName = filename.split("/")[1]
-    path = settings.MEDIA_ROOT+ "/" + filename
+    path = settings.MEDIA_ROOT + "/" + filename
     wrapper = FileWrapper(file(path))
     response = HttpResponse(wrapper.__getitem__(path),
         content_type='multipart/octet-stream')
-    response['Content-Disposition'] = 'attachment; filename='+showName
+    response['Content-Disposition'] = 'attachment; filename=' + showName
     response['Content-Length'] = os.path.getsize(path)
     return response
