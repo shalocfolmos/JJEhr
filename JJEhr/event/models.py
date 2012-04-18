f6af0425a2ca409d65a8efd4ed4eaaa699a468a8
sam.sun <shalocfolmos@gmail.com>
2012-04-18 21:06:26 +0800

    删除活动类型介绍

--- JJEhr/event/models.py
+++ JJEhr/event/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class EventType(models.Model):
     type_name = models.CharField(max_length=30, verbose_name="活动类型名称")
     type_image = models.ImageField(verbose_name="活动类型图片", blank=True, upload_to="event/event_image_%Y_%m_%d_%M_%S")
-    type_description = models.TextField(verbose_name="活动类型介绍", blank=True)
     create_date = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
