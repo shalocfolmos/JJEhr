3eaa5d0503ee3ffd2490d835e7ccd094b925dd38
sam.sun <shalocfolmos@gmail.com>
2012-07-24 16:02:07 +0800

    修改添加问卷原型

--- JJEhr/survey/create_user.py
+++ JJEhr/survey/create_user.py
@@ -10,6 +10,19 @@ user.is_active=True
 user.save()
 staff = StaffProfile(user=user,division="IT")
 staff.save()
+user=User.objects.create_user(username="sam.sun2",password="123456",email="sam.sun@jinjiang.com")
+user.is_staff=True
+user.is_active=True
+user.save()
+staff = StaffProfile(user=user,division="CC")
+staff.save()
+user=User.objects.create_user(username="sam.sun3",password="123456",email="sam.sun@jinjiang.com")
+user.is_staff=True
+user.is_active=True
+user.save()
+staff = StaffProfile(user=user,division="SALE")
+staff.save()
+
 
 
 
