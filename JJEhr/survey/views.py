2de92950609ed57d493b0492c4b2fa3591a3ef58
folmos <shalocfolmos@gmail.com>
2012-08-11 16:43:51 +0800

    comment

--- JJEhr/survey/views.py
+++ JJEhr/survey/views.py
@@ -249,6 +249,8 @@ def add_survey_result(request):
     survey_log.complete=True
     survey_log.complete_date=datetime.now()
     survey_log.save()
+    survey.total_employee_number+=1
+    survey.save()
     return render_to_response("www/thanks_page.html")
 
 
