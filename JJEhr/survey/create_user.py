582a79255f87e9d0ad3cd0b073b09a985604738d
folmos <shalocfolmos@gmail.com>
2012-08-11 21:51:29 +0800

    comment

--- JJEhr/survey/create_user.py
+++ JJEhr/survey/create_user.py
@@ -1,19 +1,19 @@
 from django.contrib.auth.models import User
 from JJEhr.survey.models import StaffProfile, SurveyLog
 
-user=User.objects.create_user(username="sam.sun1",password="123456",email="sam.sun@jinjiang.com")
+user=User.objects.create_user(username="sam.sun1",password="123456",email="sam.sun1@jinjiang.com")
 user.is_staff=True
 user.is_active=True
 user.save()
 staff = StaffProfile(user=user,division="IT")
 staff.save()
-user=User.objects.create_user(username="sam.sun2",password="123456",email="sam.sun@jinjiang.com")
+user=User.objects.create_user(username="sam.sun2",password="123456",email="sam.sun2@jinjiang.com")
 user.is_staff=True
 user.is_active=True
 user.save()
 staff = StaffProfile(user=user,division="CC")
 staff.save()
-user=User.objects.create_user(username="sam.sun3",password="123456",email="sam.sun@jinjiang.com")
+user=User.objects.create_user(username="sam.sun3",password="123456",email="sam.sun3@jinjiang.com")
 user.is_staff=True
 user.is_active=True
 user.save()
@@ -23,5 +23,5 @@ staff.save()
 
 SurveyLog.objects.get(id=1)
 
-#00042b9a9b5059bfd823c68f2a3d1a3c
+#5847d756f04e9405deab2e487b305d39
 
